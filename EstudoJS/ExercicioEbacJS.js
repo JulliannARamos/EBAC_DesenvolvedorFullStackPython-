81aa8e947af66c3c5c8f69aca4a4bf18d21b0093
Julliann Ramos <69683252+JulliannARamos@users.noreply.github.com>
2023-05-02 11:36:15 -0300

    Add files via upload

---
+++ EstudoJS/ExercicioEbacJS.js
@@ -0,0 +1,5 @@
+function calcularArea(baseAltura){
+    let calcular= base * altura
+    return calcular;
+}
+console.log(calcularArea(baseAltura(5 * 5)))
---
+++ HTMLcomJS/main.js
@@ -0,0 +1,17 @@
+const form = document.getElementById('form-deposito');
+function validaNome(nomeCompleto){
+    const nomeComoArray = nomeCompleto.split(' ');
+    return nomeComoArray.lenght >=2;
+}
+form.addEventListener('submit', function(e)
+{
+    e.preventDefault();
+
+    const nomeBeneficiario = document.getElementById('nome-beneficiario');
+    if (!validaNome(nomeBeneficiario.value)){
+        alert('nome completo')
+
+    }
+
+})
+console.log(form);
